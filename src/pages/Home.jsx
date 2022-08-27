f563da86e0640c8224c45c312dee6b4642914747
anil843324 <anilkrsingh14200@gmail.com>
2022-08-27 19:47:01 +0530

    loader added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
-import { Loader } from "@mantine/core";
+import { CirclesWithBar } from 'react-loader-spinner'
 import HomeCard from "../components/HomeCard";
 import { productList } from "../redux/productAction/productAction";
-import { useDispatch ,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Paginaton from "../components/Pagination";
+
 const Home = () => {
   // const [data, setData] = useState([]);
-  
-  const dispatch=useDispatch()
+
+  const dispatch = useDispatch()
   const data = useSelector((state) => state.productData);
 
 
@@ -26,12 +27,12 @@ const Home = () => {
 
 
   useEffect(() => {
-    
+
     dispatch(productList());
 
   }, []);
 
- 
+
 
   //min-h-screen
   return (
@@ -39,18 +40,40 @@ const Home = () => {
       <div className="max-w-[1240px] mx-auto px-4 pt-10 ">
         {/*  card  container */}
 
-        <div className=" grid  sm:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-[-0px]   ">
-        {data.slice(pagi.star, pagi.end).map((ele) => (
-                  <div className="mt-4" key={ele.id}>
-                <HomeCard ele={ele}   />
+        {
+
+          !data.length > 0 ? <div className=" grid place-items-center mt-10 h-[66vh]" > <CirclesWithBar
+            height="100"
+            width="100"
+            color="#4fa94d"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+            outerCircleColor=""
+            innerCircleColor=""
+            barColor=""
+            ariaLabel='circles-with-bar-loading'
+          /> </div> : <>
+
+            <div className=" grid  sm:grid-cols-2 lg:grid-cols-4 gap-x-6 gap-y-[-0px]   ">
+
+              {data.slice(pagi.star, pagi.end).map((ele) => (
+                <div className="mt-4" key={ele.id}>
+                  <HomeCard ele={ele} />
                 </div>
               ))}
-        </div>
-         <Paginaton
-        showPerPage={showPerPage}
-        onPaginatonChange={onPaginatonChange}
-        len={data.length}
-      />
+
+
+            </div>
+            <Paginaton
+          showPerPage={showPerPage}
+          onPaginatonChange={onPaginatonChange}
+          len={data.length}
+        />
+          </>
+        }
+
+      
       </div>
     </div>
   );
